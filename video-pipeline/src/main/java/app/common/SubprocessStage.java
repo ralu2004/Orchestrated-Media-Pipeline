4418015c7dfc20c002ef34048e05d919f47b9622
raluca_adam-admin <raluca@adamii.ro>
2026-04-07 08:21:45 +0300

    refactor(common): overload runProcess to support stderr capture

--- video-pipeline/src/main/java/app/common/SubprocessStage.java
+++ video-pipeline/src/main/java/app/common/SubprocessStage.java
@@ -5,12 +5,17 @@ import java.util.List;
 public abstract class SubprocessStage<I, O> implements PipelineStage<I, O> {
 
     protected String runProcess(List<String> command) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder(command);
-        pb.redirectErrorStream(true);
-        Process process = pb.start();
-        String output = new String(process.getInputStream().readAllBytes());
-        int exitCode = process.waitFor();
+        return runProcess(command, false);
+    }
 
+    protected String runProcess(List<String> command, boolean useStderr) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder(command);
+        if (!useStderr) pb.redirectErrorStream(true);
+        Process process = pb.start();
+        String output = useStderr
+                ? new String(process.getErrorStream().readAllBytes())
+                : new String(process.getInputStream().readAllBytes());
+        int exitCode = process.waitFor();
         if (exitCode != 0) {
             throw new PipelineException("Process failed with exit code " + exitCode + "\n" + output, getStageName());
         }
